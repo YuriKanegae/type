011389221b9f592ca48f12c2d85bcabff69dc7c2
YuriKanegae <yurikanegae@gmail.com>
2025-09-21 16:53:51 -0300

    change height calculation

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -14,7 +14,7 @@ function InfoPage(){
                 <h1>INFO</h1>
             </div>
 
-            <p>Esse projeto surgiu somente da necessidade de melhorar minhas habilidades com React e a <a href="https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API">API IndexedDB</a> dos navegadores. Caso tenha sugestões, dúvidas ou queira reportar bugs, você pode me contatar nas seguintes redes:</p>
+            <p>Esse projeto surgiu da necessidade de melhorar minhas habilidades com React e a API <a href="https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API">IndexedDB</a> dos navegadores. Caso tenha sugestões, dúvidas ou queira reportar bugs, você pode me contatar nas seguintes redes:</p>
 
             <div className="links">
                 <a href="mailto:yurikanegae@gmail.com">             <Email    width="48" height="48"/></a>
@@ -23,7 +23,9 @@ function InfoPage(){
                 <a href="https://yurikanegae.com">                  <Website  width="48" height="48"/></a>
             </div>
 
-            <p>Todo o layout e tema é inspirado no <a href="https://monkeytype.com/">MonkeyType</a>.</p>
+            <p>Todos os arquivos e configurações são salvos no banco de dados local do navegador.</p>
+
+            <p>O layout e tema é inspirado no <a href="https://monkeytype.com/">MonkeyType</a>.</p>
         </section>
     );
 }
